f09b553f09567b40292cc0a01f017ad563a40223
Vitor Henrique <vhsouzaeng@gmail.com>
2022-06-04 00:51:58 -0300

    fix: remove atributes form item model

--- apps/bff/src/domain/models/Item.ts
+++ apps/bff/src/domain/models/Item.ts
@@ -3,11 +3,9 @@ export type Item = {
     title: string
     thumbnail: string
     condition: string
-    free_shipping: boolean
     shipping: {
         free_shipping: boolean
     }
-    sold_quality: number
     price: number
     currency_id: string
     available_quantity: number
