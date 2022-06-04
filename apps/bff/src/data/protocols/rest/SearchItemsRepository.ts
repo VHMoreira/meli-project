6892a19ab88100e84fd96c840d4261cda4b6425c
Vitor Henrique <vhsouzaeng@gmail.com>
2022-06-04 03:09:29 -0300

    refactor: add field to search item repository result

--- apps/bff/src/data/protocols/rest/SearchItemsRepository.ts
+++ apps/bff/src/data/protocols/rest/SearchItemsRepository.ts
@@ -2,13 +2,19 @@ import { Item } from '@/domain/models'
 import { SearchItems } from '@/domain/usecases'
 
 export interface SearchItemsRepository {
-    search: (data: SearchItemsRepository.Params) => Promise<SearchItemsRepository.Result>
+    search(data: SearchItemsRepository.Params): Promise<SearchItemsRepository.Result>
 }
 
 export namespace SearchItemsRepository {
     export type Params = SearchItems.Params
     export type Result = {
         results: Item[]
+        filters: Array<{
+            id: string
+            values: Array<{
+                name: string
+            }>
+        }>
         available_filters: Array<{
             id: string
             values: Array<{
