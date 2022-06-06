da297222a8f141bae849e3da0b4c42a91636e21e
Vitor Henrique <vhsouzaeng@gmail.com>
2022-06-06 00:22:00 -0300

    feat: config sass

--- apps/front/src/index.tsx
+++ apps/front/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import './global.scss'
 
 const rootContainer = document.getElementById('root')
 const root = createRoot(rootContainer)
