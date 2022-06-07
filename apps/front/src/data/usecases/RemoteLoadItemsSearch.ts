e75e75fb9a16b717f24aefb4f9d51df2e7d39a8b
Vitor Henrique <vhsouzaeng@gmail.com>
2022-06-07 00:51:49 -0300

    feat: create data load itens search usecase

---
+++ apps/front/src/data/usecases/RemoteLoadItemsSearch.ts
@@ -0,0 +1,29 @@
+import { SearchResult } from "@/domain/models";
+import { LoadItemsSearch } from "@/domain/usecases/LoadItemsSearch"
+import { HttpClient, HttpStatusCode } from "../protocols/http/HttpClient";
+
+export class RemoteLoadItemsSearch implements LoadItemsSearch {
+    constructor(
+        private readonly url: string,
+        private readonly httpClient: HttpClient<RemoteLoadSurveyList.Model>
+    ) { }
+
+    async load({ query }: LoadItemsSearch.Params): Promise<SearchResult> {
+        const response = await this.httpClient.request({
+            url: `${this.url}/?q=${query}`,
+            method: 'get'
+        })
+
+        const searchResult = response.body
+
+        if (response.statusCode === HttpStatusCode.serverError) {
+            throw new Error('Unexpected Server Error')
+        }
+
+        return searchResult
+    }
+}
+
+export namespace RemoteLoadSurveyList {
+    export type Model = SearchResult
+}
