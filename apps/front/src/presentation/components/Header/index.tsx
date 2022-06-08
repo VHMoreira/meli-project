f52da63c0c0f54a77f50599ce8cca2923b6ba7e0
Vitor Henrique <vhsouzaeng@gmail.com>
2022-06-08 19:03:25 -0300

    feat: submit query on press enter

--- apps/front/src/presentation/components/Header/index.tsx
+++ apps/front/src/presentation/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { LogoMl, icSearch } from '@/presentation/assets/images'
 import Style from './styles.scss'
 
 type HandleEntryInput = React.ChangeEventHandler<HTMLInputElement>
+type HandlePressEnter = React.KeyboardEventHandler<HTMLInputElement>
 
 const Header: React.FC = () => {
     const [query, setQuery] = useState<string | null>()
@@ -18,12 +19,23 @@ const Header: React.FC = () => {
         navigate(`items?search=${query}`)
     }
 
+    const handleSubmitOnPressEnter: HandlePressEnter = ({ key }) => {
+        if (key === 'Enter') {
+            handleSubmit()
+        }
+    }
+
     return (
         <header className={Style.header}>
             <div className={Style.headerContent}>
                 <img src={LogoMl} alt="logo meli" onClick={() => navigate('/')} />
                 <div className={Style.searchbox}>
-                    <input type="text" placeholder="Nunca dejes de buscar" onChange={handleInputEntry} />
+                    <input
+                        type="text"
+                        placeholder="Nunca dejes de buscar"
+                        onChange={handleInputEntry}
+                        onKeyDown={handleSubmitOnPressEnter}
+                    />
                     <button className={Style.iconButton} onClick={handleSubmit}>
                         <img src={icSearch} alt="icon logo" />
                     </button>
